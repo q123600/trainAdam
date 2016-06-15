53b51b418876f3330065a8e862b92746cde5830c
q123600 <q123600@yahoo.com.tw>
2016-06-15 16:06:13 +0800

    Create Part 4.java

---
+++ task1/Part 4.java
@@ -0,0 +1,17 @@
+public class First {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 0 ; i < n ; i += 1){
+            for(int b = 0 ; b <= i && b < n - 1; b += 1){
+            	System.out.print("*");
+            }
+            for(int b = 1 + i; b + i < (n - 1) * 2; b += 1){
+            	System.out.print("@");
+            }
+            for(int b = (n - 1)*2 - i; b < n*2 - 1; b += 1){
+            	System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
