287725b3a2f03a027fe355fb1aa5827ee937d5ce
q123600 <q123600@yahoo.com.tw>
2016-06-16 01:12:46 +0800

    Update and rename Part 4.java to Part4.java

--- task1/Part4.java
+++ task1/Part4.java
@@ -1,4 +1,4 @@
-public class First {
+public class Part4 {
     public static void main(String[] args) {
         int n = 5;
         for(int i = 0 ; i < n ; i += 1){
@@ -6,7 +6,7 @@ public class First {
             	System.out.print("*");
             }
             for(int b = 1 + i; b + i < (n - 1) * 2; b += 1){
-            	System.out.print("@");
+            	System.out.print(" ");
             }
             for(int b = (n - 1)*2 - i; b < n*2 - 1; b += 1){
             	System.out.print("*");
